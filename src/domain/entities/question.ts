9043e62aa807e82effded5ce0bfa3d2fd799afa1
Diego Veiga <diego.veiga@xmobots.com.br>
2024-11-18 17:35:09 -0300

    Adding new fields in answer and quesion entity

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import type { Slug } from './values-object/slug'
 import { Entity } from '../../core/entities/entity'
+import type { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface QuestionProps {
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   title: string
   content: string
   slug: Slug
-  authorId: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
 class Question extends Entity<QuestionProps> {}
