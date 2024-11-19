c2feb99b7db29675f9593890c47950a4008d7260
Diego Veiga <diego.veiga@xmobots.com.br>
2024-11-19 10:58:30 -0300

    Getters and setters in entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -14,6 +14,35 @@ export class Answer extends Entity<AnswerProps> {
     return this.props.content
   }
 
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
   static create(
     props: Optional<AnswerProps, 'createdAt'>,
     id?: UniqueEntityID
