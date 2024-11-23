60dc8a419457b9269aa4ec7366ce5c2bac9bee1d
Diego Veiga <diego.veiga@xmobots.com.br>
2024-11-23 10:05:33 -0300

    create get question by slug use case

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,32 @@
+import { CreateQuestionUseCase } from './create-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { Question } from '../../enterprise/entities/question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Get question by slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to get a question by slug', async () => {
+    const newQuestion = Question.create({
+      title: 'Example question test',
+      content: 'teste',
+      authorId: new UniqueEntityID(),
+    })
+
+    inMemoryQuestionsRepository.create(newQuestion)
+
+    const { question } = await sut.execute({
+      slug: 'example-question-test',
+    })
+
+    expect(question.id).toBeTruthy()
+    expect(question.slug.value).toEqual(newQuestion.slug.value)
+  })
+})
