375dcf66324eaa011365ab479efbb07d9d1eb61e
Diego Veiga <diego.veiga@xmobots.com.br>
2024-11-25 15:57:47 -0300

    Delete answer comment use case

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,5 +1,7 @@
 import type { AnswerComment } from '../../enterprise/entities/answer-comment'
 
 export interface AnswersCommentRepository {
+  findById(id: string): Promise<AnswerComment | null>
   create(answerComment: AnswerComment): Promise<void>
+  delete(answerComment: AnswerComment): Promise<void>
 }
