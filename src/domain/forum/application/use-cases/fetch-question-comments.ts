f361f42c6aa2cdf08315cd1cce6078ae82668bd7
Diego Veiga <diego.veiga@xmobots.com.br>
2024-11-26 17:51:27 -0300

    Refactor use-cases

--- src/domain/forum/application/use-cases/fetch-question-comments.ts
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -1,3 +1,4 @@
+import { type Either, right } from '@/core/either'
 import type { QuestionComment } from '../../enterprise/entities/question-comment'
 import type { QuestionsCommentRepository } from '../repositories/question-comments-repository'
 
@@ -6,9 +7,12 @@ interface FetchQuestionCommentsUseCaseRequest {
   page: number
 }
 
-interface FetchQuestionCommentsUseCaseResponse {
-  questionComments: QuestionComment[]
-}
+type FetchQuestionCommentsUseCaseResponse = Either<
+  null,
+  {
+    questionComments: QuestionComment[]
+  }
+>
 
 export class FetchQuestionCommentsUseCase {
   constructor(private questionCommentsRepository: QuestionsCommentRepository) {}
@@ -22,6 +26,6 @@ export class FetchQuestionCommentsUseCase {
         page,
       })
 
-    return { questionComments }
+    return right({ questionComments })
   }
 }
